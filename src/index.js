bfd45130f4b21e8cb75274a6fb2ad337e8ad6217
usernamebuzz <ohminji@ewhain.net>
2022-06-26 14:58:21 +0900

    Initialize

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
