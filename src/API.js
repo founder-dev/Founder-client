8cf6b24dc57509dc9206a7e42d1bb6b826c16246
usernamebuzz <ohminji@ewhain.net>
2022-06-29 18:54:39 +0900

    Feat: use customed API

---
+++ src/API.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+/*
+    const instance = axios.create({
+    baseURL: 'https://founder/api',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    timeout: 5000,
+  });
+  
+  //필요한 API만들기
+  export const PersonalInfo = async (token) =>
+    await API.get('/mypage', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+  export const newReview = async (data) =>
+    await axios.post('/item/review', data);
+  
+  //다른 파일에서 사용예시
+  const myPage = async () => {
+    try {
+      const PersonalInfoRes = await PersonalInfo(token);*/
