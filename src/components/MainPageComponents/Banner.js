a22564519837a6fca2141062f92e6a7d1e1ade36
YoommY <seeun0792@gmail.com>
2022-07-18 19:00:30 +0900

    Style : 스와이프 pagination 구현

--- src/components/MainPageComponents/Banner.js
+++ src/components/MainPageComponents/Banner.js
@@ -1,25 +1,31 @@
 import styled from 'styled-components';
 import MainBanner from '../../assets/MainPageAssets/MainBanner.png';
-import { useRecoilValue } from 'recoil';
-import { loginState } from '../../recoil';
-import { color, fontsize, fontWeight } from '../../lib/theme';
-import { Link } from 'react-router-dom';
-import KaKaoCode from '../KakaoCode';
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css"; 
 import BannerButton from './BannerButton';
+import SwiperCore, { Navigation, Pagination, Autoplay } from "swiper";
+import "swiper/css/pagination";
+import "./Pagination.css";
+
+SwiperCore.use([Pagination, Autoplay]);
 
 const Banner = () => {
-  const isLoggedIn = useRecoilValue(loginState);
+
+  const swiperProps = {
+    spaceBetween : 20,
+    slidesPerView : 1,
+    pagination: {
+      clickable: true,
+    },
+    autoplay :{
+       delay: 2500 
+    },
+  };
 
   return (
     <BannerWrapper>
       <StyledSwiper
-      spaceBetween={50}
-      slidesPerView={1}
-      scrollbar={{ draggable: true }}
-      navigation
-      pagination={{ clickable: true }}
+      {...swiperProps}
     >
         <SwiperSlide>
           <BannerImage  src={MainBanner} />
@@ -50,5 +56,5 @@ const BannerImage = styled.img`
 
 const StyledSwiper = styled(Swiper)`
   width: 1441px;
-  height: 280px;
+  height: 300px;
 `;
