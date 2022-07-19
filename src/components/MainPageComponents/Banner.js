7ff83b5fe16f501562efd4745f18a139a735d6c2
usernamebuzz <ohminji@ewhain.net>
2022-07-19 13:24:56 +0900

    Chore: Update item tag

--- src/components/MainPageComponents/Banner.js
+++ src/components/MainPageComponents/Banner.js
@@ -1,45 +1,42 @@
 import styled from 'styled-components';
 import MainBanner from '../../assets/MainPageAssets/MainBanner.png';
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css"; 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 import BannerButton from './BannerButton';
-import SwiperCore, { Navigation, Pagination, Autoplay } from "swiper";
-import "swiper/css/pagination";
-import "./Pagination.css";
+import SwiperCore, { Navigation, Pagination, Autoplay } from 'swiper';
+import 'swiper/css/pagination';
+import './Pagination.css';
 
 SwiperCore.use([Pagination, Autoplay]);
 
 const Banner = () => {
-
   const swiperProps = {
-    spaceBetween : 20,
-    slidesPerView : 1,
+    spaceBetween: 20,
+    slidesPerView: 1,
     pagination: {
       clickable: true,
     },
-    autoplay :{
-       delay: 2500 
+    autoplay: {
+      delay: 2500,
     },
   };
 
   return (
     <BannerWrapper>
-      <StyledSwiper
-      {...swiperProps}
-    >
+      <StyledSwiper {...swiperProps}>
         <SwiperSlide>
-          <BannerImage  src={MainBanner} />
-          <BannerButton/>
+          <BannerImage src={MainBanner} />
+          <BannerButton />
         </SwiperSlide>
         <SwiperSlide>
-          <BannerImage  src={MainBanner} />
-          <BannerButton/>
+          <BannerImage src={MainBanner} />
+          <BannerButton />
         </SwiperSlide>
         <SwiperSlide>
-          <BannerImage  src={MainBanner} />
-          <BannerButton/>
+          <BannerImage src={MainBanner} />
+          <BannerButton />
         </SwiperSlide>
-     </StyledSwiper>
+      </StyledSwiper>
     </BannerWrapper>
   );
 };
@@ -50,11 +47,11 @@ const BannerWrapper = styled.div`
   position: relative;
 `;
 const BannerImage = styled.img`
-  width: 1441px;
+  width: 1440px;
   height: 280px;
 `;
 
 const StyledSwiper = styled(Swiper)`
-  width: 1441px;
+  width: 1440px;
   height: 280px;
 `;
