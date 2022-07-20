0b04da6013d49ce108a2991dd53dd4aff12a0738
YoommY <seeun0792@gmail.com>
2022-07-20 18:32:00 +0900

    Feat : api를 위해 .env 설정

--- src/components/KakaoCode.js
+++ src/components/KakaoCode.js
@@ -5,7 +5,11 @@ function KaKaoCode (){
   const kakaologin = async () => {
     try {
       const response = await axios.get(
-        'https://found-er.co.kr/api/auth/kakao/signin/'
+        'https://found-er.co.kr/api/auth/kakao/signin/',
+
+        {
+          headers: {'Access-Control-Allow-Origin': "*"}, // 이래도 안됨ㅋ
+        },
       );
       console.log(response.data);
     } catch (e) {
