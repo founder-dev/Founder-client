fa561bf7880e15f3c2894d581b64ea36174a775d
usernamebuzz <ohminji@ewhain.net>
2022-08-03 18:47:49 +0900

    Chore: Set login state false

--- src/components/KakaoCode.js
+++ src/components/KakaoCode.js
@@ -5,13 +5,7 @@ function KaKaoCode() {
   const kakaologin = async () => {
     try {
       const response = await axios.get(
-        'https://found-er.co.kr/api/auth/kakao/signin',
-
-        {
-          headers: {
-            'Access-Control-Allow-Origin': '*',
-          }, // 이래도 안됨ㅋ
-        }
+        'https://found-er.co.kr/api/auth/kakao/signin'
       );
       console.log(response.data);
       loginState(true);
