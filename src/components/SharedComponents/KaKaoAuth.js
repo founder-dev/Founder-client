f32f3522ee355f673755eb959fc714267661144c
usernamebuzz <ohminji@ewhain.net>
2022-08-05 18:12:10 +0900

    Chore: change redirect

--- src/components/SharedComponents/KaKaoAuth.js
+++ src/components/SharedComponents/KaKaoAuth.js
@@ -1,5 +1,5 @@
 import * as config from '../../config';
 
 const CLIENT_ID = config.CLIENT_ID;
-const REDIRECT_URI = 'https://founder-client-1.vercel.app/kakao';
+const REDIRECT_URI = 'https://founder-client-3.vercel.app/kakao';
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
